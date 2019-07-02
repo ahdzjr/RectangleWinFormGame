f4f4ce2a1b326c6743ce3874be1cbd7f88ea4f3c
Albert Hernandez Jr <albert.hernandez.jr@outlook.com>
2019-07-02 18:22:37 -0500

    first commit

---
+++ RectangleWinFormsGame/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace RectangleWinFormsGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+             
+            // panel1
+            
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(5, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(730, 344);
+            this.panel1.TabIndex = 0;
+            
+            // UpButton
+            
+            this.UpButton.Location = new System.Drawing.Point(326, 357);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(81, 24);
+            this.UpButton.TabIndex = 1;
+            this.UpButton.Text = "Up";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            
+            // LeftButton
+            
+            this.LeftButton.Location = new System.Drawing.Point(279, 387);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(81, 24);
+            this.LeftButton.TabIndex = 2;
+            this.LeftButton.Text = "Left";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            
+            // RightButton
+            
+            this.RightButton.Location = new System.Drawing.Point(366, 387);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(81, 24);
+            this.RightButton.TabIndex = 3;
+            this.RightButton.Text = "Right";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            
+            // DownButton
+            
+            this.DownButton.Location = new System.Drawing.Point(326, 417);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(81, 24);
+            this.DownButton.TabIndex = 4;
+            this.DownButton.Text = "Down";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            
+            // Form1
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 446);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Rectangle Game";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Rectangle1;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Label Rectangle2;
+    }
+}
+
---
+++ RectangleWinFormsGame/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RectangleWinFormsGame
+{
+    public partial class Form1 : Form
+    {
+        static int StartX = 20;
+        static int StartY = 20;
+        bool KeyReached = false;
+        bool GoalReached = false;
+        Rectangle PlayerRectangle = new Rectangle(StartX, StartY, 25, 25);
+        Rectangle GoalRectangle = new Rectangle(670, 285, 50, 50);
+        Rectangle KeyRectangle = new Rectangle(670, 20, 10, 10);
+        Pen GoldPen = new Pen(Color.Gold);
+        Pen BluePen = new Pen(Color.Blue);
+        Pen RedPen = new Pen(Color.Red);
+
+        Rectangle EWall1 = new Rectangle(60, 275, 25, 75);
+        Pen LightBlue = new Pen(Color.LightBlue);
+        Rectangle EWall2 = new Rectangle(60, 0, 25, 200);
+        Rectangle EWall3 = new Rectangle(600, 60, 125, 20);
+        Rectangle EWall4 = new Rectangle(550, 150, 20, 125);
+        Rectangle PoisonPond = new Rectangle(200, 175, 300, 150);
+        Pen Purple = new Pen(Color.Purple);
+        Rectangle Spikes = new Rectangle(700, 0, 25, 275);
+        Pen White = new Pen(Color.White);
+        Rectangle Spikes2 = new Rectangle(175, 100, 350, 25);
+        Rectangle Spikes3 = new Rectangle(175, 0, 350, 25);
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Welcome to my Rectangle WinForms Game.\n" +
+            "\nUse the buttons to move the blue rectangle to the red rectangle goal." +
+            "\nRemember to avoid obstacles!" +
+            "\nYou must get the key(gold rectangle) to enter the goal." +
+            "\nEnjoy!");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.DrawRectangle(BluePen, PlayerRectangle);
+            g.DrawRectangle(RedPen, GoalRectangle);
+            g.DrawRectangle(GoldPen, KeyRectangle);
+
+            g.DrawRectangle(LightBlue, EWall1);
+            g.DrawRectangle(LightBlue, EWall2);
+            g.DrawRectangle(LightBlue, EWall3);
+            g.DrawRectangle(LightBlue, EWall4);
+            g.DrawRectangle(Purple, PoisonPond);
+            g.DrawRectangle(White, Spikes);
+            g.DrawRectangle(White, Spikes2);
+            g.DrawRectangle(White, Spikes3);
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            PlayerRectangle.Y += 25;
+            RedrawRectangles();
+            CheckKey();
+            CheckGoal();
+            CheckAlive();
+        }
+
+        private void RedrawRectangles()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Refresh();
+            g.DrawRectangle(BluePen, PlayerRectangle);
+            g.DrawRectangle(RedPen, GoalRectangle);
+            g.DrawRectangle(GoldPen, KeyRectangle);
+
+            g.DrawRectangle(LightBlue, EWall1);
+            g.DrawRectangle(LightBlue, EWall2);
+            g.DrawRectangle(LightBlue, EWall3);
+            g.DrawRectangle(LightBlue, EWall4);
+            g.DrawRectangle(Purple, PoisonPond);
+            g.DrawRectangle(White, Spikes);
+            g.DrawRectangle(White, Spikes2);
+            g.DrawRectangle(White, Spikes3);
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            PlayerRectangle.Y -= 25;
+            RedrawRectangles();
+            CheckKey();
+            CheckGoal();
+            CheckAlive();
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            PlayerRectangle.X -= 25;
+            RedrawRectangles();
+            CheckKey();
+            CheckGoal();
+            CheckAlive();
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            PlayerRectangle.X += 25;
+            RedrawRectangles();
+            CheckKey();
+            CheckGoal();
+            CheckAlive();
+        }
+
+        private void CheckAlive()
+        {
+            if (PlayerRectangle.IntersectsWith(EWall1))
+            {
+                MessageBox.Show("You were electrocuted to death.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(EWall2))
+            {
+                MessageBox.Show("You were electrocuted to death.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(EWall3))
+            {
+                MessageBox.Show("You were electrocuted to death.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(EWall4))
+            {
+                MessageBox.Show("You were electrocuted to death.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(PoisonPond))
+            {
+                MessageBox.Show("You fell into the poison and died.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(Spikes))
+            {
+                MessageBox.Show("You fell onto some spikes and died.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(Spikes2))
+            {
+                MessageBox.Show("You fell onto some spikes and died.");
+                Reset();
+            }
+
+            if (PlayerRectangle.IntersectsWith(Spikes3))
+            {
+                MessageBox.Show("You fell onto some spikes and died.");
+            }
+        }
+        private void CheckGoal()
+        {
+            if (KeyReached && !GoalReached && PlayerRectangle.IntersectsWith(GoalRectangle))
+            {
+                GoalReached = true;
+                MessageBox.Show("You Win!");
+                Reset();
+            }
+            else if (!KeyReached && !GoalReached && PlayerRectangle.IntersectsWith(GoalRectangle))
+            {
+                MessageBox.Show("You need a key to open the goal!");
+            }
+        }
+
+        private void CheckKey()
+        {
+            if (!KeyReached && PlayerRectangle.IntersectsWith(KeyRectangle))
+            {
+                KeyReached = true;
+                MessageBox.Show("You got the key!");
+            }
+        }
+
+        private void Reset()
+        {
+            //key v goes back
+            KeyReached = false;
+            //player rec goes back
+            PlayerRectangle.X = StartX;
+            PlayerRectangle.Y = StartY;
+            RedrawRectangles();
+            //reset goal reached
+            GoalReached = false;
+        }
+    }
+}
